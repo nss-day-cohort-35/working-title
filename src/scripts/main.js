c17e1322760b9376dea98c1d4cc1f1bcb02defb9
Steve Brownlee <chortlehoort@gmail.com>
2019-08-14 22:56:00 -0500

    Added Webpack boilerplate code

---
+++ src/scripts/main.js
@@ -0,0 +1,5 @@
+const message = "Time to build an application that gives you all the information you need in a Nutshell"
+
+document.querySelector("#container").innerHTML = `<h1>${message}</h1>`
+
+console.log(message)
