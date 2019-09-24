00176fe710f5cf864b3aa2ceaa0f73faf83daa46
Stephen Senft <stephensenft21@gmail.com>
2019-09-24 13:01:39 -0500

    changes made to chatDOMinjector

--- src/scripts/messages/messagesWebComponent.js
+++ src/scripts/messages/messagesWebComponent.js
@@ -33,27 +33,27 @@ const messagesComponentMaker = {
             <div class="inline">
             <h5>${object.date}
             </h5>
-            <h3>${object.userId.email}</h3>
-            <p> - </p> <p>${object.text}</p>
+            <h3 id =" messagesEmail${object.id}">${object.userId.email}</h3>
+            <p> - </p> <p id ="messagesText${object.id}>${object.text}</p>
             </div>
                <div><button class="messagesDelete margin" value="${object.id}">Delete</button>
-               <button class="messagesEdit" value="${object.userId}">Edit</button></div>
+               <button class="messagesEdit" value="${object.userId.id}">Edit</button></div>
         </section>
         
         `; // The delete button stores the id of the messages, the edit button stores the id of the person. Since they itterate the same in a loop, i can easily call them both.
   },
 
-  makeUneditablemessagesArticle: function(object) {
-    return `<section>
-        <input type="hidden" class="messagesIdHolder" value="" />
-        <input type="hidden" class="messagesuserIdHolder" value="" />
-        <div class="inline"><h4 class = "margin">${object.title}</h4> <h5>${object.date}</h5></div>
-        <p>${object.summary}</p>
-        <a href="${object.url}"><p>${object.url}</p></a>
-        </section>
+//   makeUneditableMessagesArticle: function(object) {
+//     return `<section>
+//         <input type="hidden" class="messagesIdHolder" value="" />
+//         <input type="hidden" class="messagesuserIdHolder" value="" />
+//         <div class="inline"><h4 class = "margin">${object.title}</h4> <h5>${object.date}</h5></div>
+//         <p>${object.summary}</p>
+//         <a href="${object.url}"><p>${object.url}</p></a>
+//         </section>
         
-        `; // The delete button stores the id of the messages, the edit button stores the id of the person. Since they itterate the same in a loop, i can easily call them both.
-  }
+//         `; // The delete button stores the id of the messages, the edit button stores the id of the person. Since they itterate the same in a loop, i can easily call them both.
+//   }
 };
 
 export default messagesComponentMaker;
